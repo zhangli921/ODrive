87be1a654f053020c763211e37e67e583369dc39
zhangli921 <zhangli921@gmail.com>
2023-01-03 09:40:06 +0800

    更新判断node_id位数的算法

--- Firmware/communication/ascii_protocol.cpp
+++ Firmware/communication/ascii_protocol.cpp
@@ -113,8 +113,13 @@ void AsciiProtocol::process_line(cbufptr_t buffer) {
     if(sscanf(cmd, "n %u ", &node_id) < 1) {
         return;
     }    
-    size_t node_id_digits = (node_id < 10) ? 1 : ((node_id < 100) ? 2 : ((node_id < 1000) ? 3 : 4));  
-    char *cmd2 = cmd + 3 + node_id_digits; //!! 跳过节点id的字符
+    //!! 跳过节点id字符
+    char *cmd2 = cmd + 3;
+    unsigned int number = node_id;
+    while (number != 0) {	
+		cmd2++;
+		number = number / 10;
+	}   
     for (auto& axis : axes) {
         if (axis.config_.uart.node_id == node_id) {
             // check incoming packet type           
